7f3d3df7efec9dfe6c1493b91aff3b94352a404f
gamsayeon <aud4551@naver.com>
2025-10-26 19:26:41 +0900

    test: 최종 우승자 출력 테스트 작성(TDD Red)

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,12 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -25,7 +25,8 @@ class ApplicationTest extends NsTest {
                 positionsOutput = positionsOutput.replace("\r\n", "\n");
                 assertThat(positionsOutput).contains("실행 결과\n"
                         + "pobi : -\n"
-                        + "woni :");
+                        + "woni :\n"
+                        + "최종 우승자 : pobi\n");
             },
             MOVING_FORWARD, STOP
         );
